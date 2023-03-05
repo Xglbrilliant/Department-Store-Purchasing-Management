6edbd5cd9a5ddc5cb8febcb0e32f706c3052b5ca
Xglbrilliant <brilliant2125@163.com>
2023-03-05 12:59:23 +0800

    add information on the right side of the homepage

--- src/views/layout/content/Index.vue
+++ src/views/layout/content/Index.vue
@@ -7,6 +7,22 @@
         <span v-if="!isCollapse" class="iconfont icon-right-indent" @click="changMenu"></span>
         <span v-else class="iconfont icon-left-indent" @click="changMenu"></span>
       </div>
+      <!-- 右侧内容--时间 登录信息等 -->
+      <div class="right">
+        <span>{{ time }}</span> | 
+        <el-dropdown>
+          <span class="el-dropdown-link" style="color:#fff">
+            语言环境<i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>中文</el-dropdown-item>
+            <el-dropdown-item>English</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+         | 
+        <span>欢迎 admin</span> | 
+        <span class="el-icon-switch-button icon"></span>
+      </div>
     </div>
     <!-- 内容 -->
     <div class="wrapper">
@@ -15,24 +31,47 @@
   </div>
 </template>
 <script>
-// import { mapMutations } from 'vuex'
+import dayjs from 'dayjs'
+// import { mapMutations, mapState } from 'vuex'
 
 export default {
   props: ['isCollapse'],  //父传子props  layout/Index.vue->menu/Index.vue和content/Index.vue
-  components: {
-    
-  },
   data() {
     return {
-
+      time: ''
     };
   },
+  // computed:{
+  //   ...mapState('login',['userinfo'])
+  // },
   methods: {
     // ...mapMutations('menu',['removeMenuList']),
+    // ...mapMutations('login',['removeUser']),
     changMenu() {
       this.$emit('changeShow')  //子传父自定义事件  content/Index.vue->layout/Index.vue
     },
+    // changeLang(val){
+    //   console.log(val);
+    //   console.log(this);
+    //   this.$i18n.locale=val;
+    // },
+    //退出登录  1. 删除vuex登录信息  2. 跳转登录界面  3. 清空菜单导航
+    // loginOut(){
+    //   //进入登录界面
+    //   this.$router.replace('/login')
+    //   //清空菜单导航
+    //   this.removeMenuList();
+    //   //清空登录
+    //   this.removeUser()
+    //   // console.log('----退出登录----------------');
+    //   localStorage.removeItem('info') //删除有问题 需要单独删除模块名称  vuex持久化注意的 插件--
+    //   localStorage.removeItem('login')
+    // }
   },
+  created() {
+    // console.log(dayjs().format('YYYY-MM-DD HH:mm:ss'));
+    this.time = dayjs().format('YYYY-MM-DD HH:mm:ss');
+  }
 };
 </script>
 <style lang='less' scoped>
@@ -43,11 +82,21 @@ export default {
   line-height: 50px;
   display: flex;
   .open {
-    // flex: 1;
+    flex: 1;
 
     .iconfont {
       font-size: 22px;
       cursor: pointer;
+    }
+  }
+  .right {
+    width: 380px;
+    font-size: 12px;
+    span{
+      padding:0 4px;
+    }
+    .icon{
+      font-size: 16px;
     }
   }
 }
