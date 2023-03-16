b5868a458da1d7a574c14f8204a142c4708825fd
Xglbrilliant <brilliant2125@163.com>
2023-03-16 21:27:18 +0800

    export the namespaced and add plugins

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import product from './modules/product.js'
+import createPersistedstate   from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
@@ -13,5 +15,13 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-  }
+    product
+  },
+  //plugins:[] vuex插件-持久化 数组语法 多个插件名称
+  plugins:[
+    createPersistedstate({
+      key:'info',//存储vuex数据的任意键名--本地存储里面 localStorage 
+      paths:['product','login'],//存储的模块名称一级全局state数据  不写默认存储所有内容
+    }),
+  ]
 })
