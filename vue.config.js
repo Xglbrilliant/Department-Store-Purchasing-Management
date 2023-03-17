a02955cffe920f8dd8104734f5fa4621b047e9d8
Xglbrilliant <brilliant2125@163.com>
2023-03-17 19:47:04 +0800

    fix the bug:WebSocket connection to ‘ws://xxx.xxx.x.xxxx:8080/ws‘failed:

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,21 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    // host: '0.0.0.0',
+    // // https:true,
+    // port: 6103,
+    // client: {
+    //   webSocketURL: 'ws://0.0.0.0:6103/ws',
+    // },
+    // headers: {
+    //   'Access-Control-Allow-Origin': '*',
+    // },
     proxy: {
       '/api': {
         target: 'http://localhost:7788/',//目标地址 请求的后台接口 
-        ws: true,
+        ws: false,
         changeOrigin: true,
-        pathRewrite:{//路径重写
+        pathRewrite:{//路径重写 
           "^/api":''
         }
       },
