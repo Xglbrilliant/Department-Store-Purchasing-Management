9da1f0268d6e52e9cbd649a7815bebb8bb5f996d
Xglbrilliant <brilliant2125@163.com>
2023-03-20 01:19:06 +0800

    use permission

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import './plugins/element.js'
 import 'normalize.css'
 import '@/assets/css/base.css'
+import './permission'
 import api from './api'
 Vue.prototype.$api = api;
 
