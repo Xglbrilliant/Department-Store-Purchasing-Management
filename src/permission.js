74e423adf497ddc2d39cc3bb4ff54d8760cf0183
Xglbrilliant <brilliant2125@163.com>
2023-03-20 12:51:59 +0800

    arrange the front route first

--- src/permission.js
+++ src/permission.js
@@ -11,14 +11,16 @@ import router from "@/router";
 import store from "@/store";
 
 router.beforeEach((to, from, next) => {
+  // console.log("路由守卫", to); //{to.matched: []}
   //1、首先 判断进入的路由界面是否需要登录  不需要登录直接进入
   if (to.matched.length == 0 || to.matched.some((ele) => ele.meta.isLogin)) {
     if (store.state.login.userinfo.token) {//2、判断是否登录
       //3、判断当前存储的vuex里面是否已经有动态导航了.如果没有菜单导航则需要获取菜单导航
-      if (!store.state.menu.dyMenuList.length) {//没有导航
+      if (store.state.menu.dyMenuList.length != 0) {//有导航
+        next();
+      }else {
         store.dispatch("menu/getMenuList");//获取导航
       }
-      next();
     } else {
       next("/login");
     }
