b80396e3d140aa011df985bc7dab261ae9aaa774
Xglbrilliant <brilliant2125@163.com>
2023-03-20 13:57:09 +0800

    get route matching result

--- src/store/modules/menu.js
+++ src/store/modules/menu.js
@@ -4,7 +4,8 @@ import {permission} from '@/api/index'
 //导入前端定义好的完整的路由信息
 import { menu } from '@/router/menu'
 //导入路由 和基本的路由信息
-import router,{baseRoutes} from '@/router/index'
+import router,{ baseRoutes } from '@/router/index'
+import { rulesMenu } from '@/utils/common'
 export default {
     namespaced: true,
     state: {
@@ -28,6 +29,9 @@ export default {
           let res = await permission({token:rootState.login.userinfo.token})
           console.log('后端返回的导航菜单内容:----',res.data.data);
           console.log('前端定义的导航菜单内容-----',menu);
+          //定义一个方法来处理前后端的菜单导航数组：根据后端返回的导航标识 匹配对应的真实的路由菜单导航
+          let myMenu= rulesMenu(menu,res.data.data);//返回匹配好的菜单导航
+          console.log('处理好的菜单导航',myMenu);
         }
     }
   };
