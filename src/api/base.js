13391d245dfafeffb57e830e11e45df3dfad916c
Xglbrilliant <brilliant2125@163.com>
2023-03-21 00:26:08 +0800

    split api methods

--- src/api/base.js
+++ src/api/base.js
@@ -2,34 +2,35 @@
 //环境变量
 // let api = process.env.VUE_APP_BASE_URL;// /api
 const base ={
-    //首页
-    totalInfo:'/api/home/dataCount',//首页统计数据
-    orderinfo:"/api/home/orderinfo",//订单数据
-    format:"/api/home/format",//折线数据
-    //产品管理
-    projectList: "/api/goods/projectList",//产品列表
-    search: "/api/goods/search",//搜索接口
-    deleteItemById: "/api/goods/deleteItemById",//删除商品操作
-    selectItemCategoryByParentId: "/api/goods/itemCategory/selectItemCategoryByParentId",//商品类目
-    uploadUrl: "/api/upload",//图片上传接口
-    insertTbItem: "/api/goods/item/insertTbItem",//添加商品
-    batchDelete: "/api/goods/batchDelete",//批量删除
-    updateTbItem: "/api/goods/item/updateTbItem",//修改商品
-    //产品分类
-    itemCategory: "/api/itemCategory",//产品类别
-    insertItemCategory: "/api/itemCategory/insertItemCategory", //新增子类目录名称 cid name
-    deleteContentCategoryById: "/api/content/deleteContentCategoryById", //删除类名 id
-    updateCategory: "/api/itemCategory/updateCategory", //修改类目 id name
-    insertCategory: "/api/itemCategory/insertCategory", //新增一级分类名称 name
+    // //首页
+    // totalInfo:'/api/home/dataCount',//首页统计数据
+    // orderinfo:"/api/home/orderinfo",//订单数据
+    // format:"/api/home/format",//折线数据
+    // //产品管理
+    // projectList: "/api/goods/projectList",//产品列表
+    // search: "/api/goods/search",//搜索接口
+    // deleteItemById: "/api/goods/deleteItemById",//删除商品操作
+    // selectItemCategoryByParentId: "/api/goods/itemCategory/selectItemCategoryByParentId",//商品类目
+    // uploadUrl: "/api/upload",//图片上传接口
+    // insertTbItem: "/api/goods/item/insertTbItem",//添加商品
+    // batchDelete: "/api/goods/batchDelete",//批量删除
+    // updateTbItem: "/api/goods/item/updateTbItem",//修改商品
+    // //产品分类
+    // itemCategory: "/api/itemCategory",//产品类别
+    // insertItemCategory: "/api/itemCategory/insertItemCategory", //新增子类目录名称 cid name
+    // deleteContentCategoryById: "/api/content/deleteContentCategoryById", //删除类名 id
+    // updateCategory: "/api/itemCategory/updateCategory", //修改类目 id name
+    // insertCategory: "/api/itemCategory/insertCategory", //新增一级分类名称 name
+    
     //订单管理
     orderList:"/api/order/list",//订单列表 page
     changeStatus:"/api/order/changeStatus",//订单汇总 
     collect:"/api/order/collect",//汇总清单列表 page
     cancel:'/api/order/cancel',//撤销汇总 id
     detail:"/api/order/list/detail",//订单详情  id
-    //登录
-    login: "/api/login", //登录接口 user pwd---post
-    permission: "/api/permission", //用户权限 token
+    // //登录
+    // login: "/api/login", //登录接口 user pwd---post
+    // permission: "/api/permission", //用户权限 token
 
 
     //打包
--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 //公共的请求方法
 import base from "./base";
 import axios from "@/utils/request";
-// import {login,permission} from './login/login'
-// import product from './product'
+import {login,permission} from './login/login'
+import product from './product'
 const api = {
   /* 首页数据统计 */
   totalInfo() {
@@ -16,68 +16,68 @@ const api = {
   format() {
     return axios.get(base.format);
   },
-  //产品信息接口---
-  // ...product,
+  // 产品信息接口---
+  ...product,
   
-  // 产品列表 Page
-  projectList(params) {
-    return axios.get(base.projectList, { params });
-  },
+  // // 产品列表 Page
+  // projectList(params) {
+  //   return axios.get(base.projectList, { params });
+  // },
 
-  // 搜索接口 params={search=xxx}
-  search(params) {
-    return axios.get(base.search, { params });
-  },
+  // // 搜索接口 params={search=xxx}
+  // search(params) {
+  //   return axios.get(base.search, { params });
+  // },
 
-  // 删除接口 params={search=xxx}
-  deleteItemById(params) {
-    return axios.get(base.deleteItemById, { params });
-  },
+  // // 删除接口 params={search=xxx}
+  // deleteItemById(params) {
+  //   return axios.get(base.deleteItemById, { params });
+  // },
 
-  //商品类目  params={type: cid}
-  selectItemCategoryByParentId(params) {
-    return axios.get(base.selectItemCategoryByParentId, { params });
-  },
+  // //商品类目  params={type: cid}
+  // selectItemCategoryByParentId(params) {
+  //   return axios.get(base.selectItemCategoryByParentId, { params });
+  // },
 
-  //添加商品  parms={title, image, sellPoint, price, cid, category, num, descs, paramsInfo}
-  insertTbItem(params) {
-    return axios.get(base.insertTbItem, { params });
-  },
+  // //添加商品  parms={title, image, sellPoint, price, cid, category, num, descs, paramsInfo}
+  // insertTbItem(params) {
+  //   return axios.get(base.insertTbItem, { params });
+  // },
 
-  //批量删除  {ids:字符串--id}
-  batchDelete(params) {
-    return axios.get(base.batchDelete, { params });
-  },
+  // //批量删除  {ids:字符串--id}
+  // batchDelete(params) {
+  //   return axios.get(base.batchDelete, { params });
+  // },
 
-  //修改商品  parms={id, title, image, sellPoint, price, cid, category, num, descs, paramsInfo}
-  updateTbItem(params) {
-    return axios.get(base.updateTbItem, { params });
-  },
+  // //修改商品  parms={id, title, image, sellPoint, price, cid, category, num, descs, paramsInfo}
+  // updateTbItem(params) {
+  //   return axios.get(base.updateTbItem, { params });
+  // },
 
-  //产品列表分类
-  itemCategory() {
-    return axios.get(base.itemCategory);
-  },
+  // //产品列表分类
+  // itemCategory() {
+  //   return axios.get(base.itemCategory);
+  // },
 
-  //新增子级类目名称  parms={cid, name}
-  insertItemCategory(params) {
-    return axios.get(base.insertItemCategory, { params });
-  },
+  // //新增子级类目名称  parms={cid, name}
+  // insertItemCategory(params) {
+  //   return axios.get(base.insertItemCategory, { params });
+  // },
 
-  //删除类目  parms={id}
-  deleteContentCategoryById(params) {
-    return axios.get(base.deleteContentCategoryById, { params });
-  },
+  // //删除类目  parms={id}
+  // deleteContentCategoryById(params) {
+  //   return axios.get(base.deleteContentCategoryById, { params });
+  // },
 
-  //修改类目名称  parms={id, name}
-  updateCategory(params) {
-    return axios.get(base.updateCategory, { params });
-  },
+  // //修改类目名称  parms={id, name}
+  // updateCategory(params) {
+  //   return axios.get(base.updateCategory, { params });
+  // },
 
-  //新增一级分类名称  parms={name}
-  insertCategory(params) {
-    return axios.get(base.insertCategory, { params });
-  },
+  // //新增一级分类名称  parms={name}
+  // insertCategory(params) {
+  //   return axios.get(base.insertCategory, { params });
+  // },
 
 
   /*  订单列表 */
@@ -111,28 +111,30 @@ const api = {
   getDetail(params) {
     return axios.get(base.detail, { params });
   },
+  // //登录
+  // // login,params={user: '',pwd: ''}
+  // login(params) {
+  //   return axios.post(base.login, params);//get传参要加大括号，post则不需要
+  // },
+  // /* 
+  //     用户权限--返回导航内容
+  //     params={token:''}
+  //   */
+  // permission(params) {
+  //   return axios.get(base.permission, { params });
+  // }
   //登录
-  // login,params={user: '',pwd: ''}
-  login(params) {
-    return axios.post(base.login, params);//get传参要加大括号，post则不需要
-  },
-  /* 
-      用户权限--返回导航内容
-      params={token:''}
-    */
-  permission(params) {
-    return axios.get(base.permission, { params });
-  }
+  login,
 
 
 };
 //单个导出
-export function permission(params){
-  return axios.get(base.permission,{
-    params
-  })
-}
-// export { permission }
+// export function permission(params){
+//   return axios.get(base.permission,{
+//     params
+//   })
+// }
+export { permission }
 
 //导出对象 api身上挂载了很多方法
 export default api;
