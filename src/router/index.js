47a0ede907f5da559ad344cd46ec478a1dff1688
Xglbrilliant <brilliant2125@163.com>
2023-03-20 12:53:01 +0800

    split route navigation

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Home from '@/views/home/home.vue'
 
 Vue.use(VueRouter)
 //路由信息 this.$route.matched=[{一级路由信息},{二级路由信息},...{当前路由信息}] 
-const routes = [
+export const baseRoutes = [
   {
     path: '/',
     component: () => import('@/views/layout/Index.vue'),
@@ -21,126 +21,146 @@ const routes = [
           title: '首页',
         }
       },
-      {
-        path: '/product',
-        name: 'product',
-        component: () => import('@/views/product/Index.vue'),
-        redirect: '/product/list',//点击面包屑跳转时重定向
-        meta: {
-          title: '产品管理',
-        },
-        children: [
-          {
-            path: 'list',
-            name: 'list',
-            component: () => import('@/views/product/list/Index.vue'),
-            meta: {
-              title: '产品列表',
-            },
-          },
-          {
-            path: 'category',  //加/跳转时会从站点的根目录开始寻找，而不加/则会从当前路径开始寻找
-            name: 'category',
-            component: () => import('@/views/product/category/Index.vue'),
-            meta: {
-              title: '产品分类',
-            },
-          },
-          {
-            path: 'add-product',  //商品添加-编辑-查看
-            name: 'ProductPage',
-            component: () => import('@/views/product/list/AddProduct.vue'),
-            meta: {  //配置高亮标识
-              activeMenu: '/product/list',
-              title: '商品信息编辑'
-            }
-          },
-        ]
-      },
-      {
-        path: '/order',
-        name: 'order',
-        component: () => import('@/views/order/Index.vue'),
-        redirect: '/order/collect',
-        meta: {
-          title: '订单管理',
-        },
-        children: [
-          {
-            path: 'collect',
-            name: 'collect',
-            component: () => import('@/views/order/collect/Index.vue'),
-            meta: {
-              title: '订单汇总',
-            },
-          },
-          {
-            path: 'contract',
-            name: 'contract',
-            component: () => import('@/views/order/contract/Index.vue'),
-            meta: {
-              title: '订单审核',
-            },
-          },
-          {
-            path: 'order-list',
-            name: 'orderList',
-            component: () => import('@/views/order/list/Index.vue'),
-            meta: {
-              title: '订单列表',
-            },
-          },
-        ]
-      },
-      {
-        path: '/advert',
-        name: 'advert',
-        component: () => import('@/views/advert/Index.vue'),
-        // redirect: '/advert/list',
-        meta: {
-          title: '广告管理',
-        },
-        children: [
-          {
-            path: 'advert-list',
-            name: 'advertList',
-            component: () => import('@/views/advert/list/Index.vue'),
-            meta: {
-              title: '广告列表',
-            },
-          },
-        ]
-      },
-      { 
-        path:'/system',
-        name:'system',
-        component: () => import('@/views/SystemMange/Index.vue'),
-        redirect:'/system/role',
-        meta:{
-          title:"系统管理",
-        },
-        children:[
-          {
-            path:'role',
-            name:'role',
-            component: () => import('@/views/SystemMange/role/Index.vue'),
-            meta:{
-              title:"角色管理",
-            },
-          },
-          {
-            path:'department',
-            name:'department',
-            component: () => import('@/views/SystemMange/department/Index.vue'),
-            meta:{
-              title:"部门管理",
-            },
-          }
-        ]
-    
-      },
     ]
   },
+]
+export const routes = [
+  // {
+  //   path: '/',
+  //   component: () => import('@/views/layout/Index.vue'),
+  //   meta: {
+  //     title: '万民百货管理系统',
+  //     isLogin:true,//是否需要登录
+  //   },
+  //   children: [
+  //     {
+  //       path: '/',
+  //       name: 'home',
+  //       component: Home,  //首页展示所以不使用懒加载或异步组件
+  //       meta: {
+  //         title: '首页',
+  //       }
+  //     },
+      // {
+      //   path: '/product',
+      //   name: 'product',
+      //   component: () => import('@/views/product/Index.vue'),
+      //   redirect: '/product/list',//点击面包屑跳转时重定向
+      //   meta: {
+      //     title: '产品管理',
+      //   },
+      //   children: [
+      //     {
+      //       path: 'list',
+      //       name: 'list',
+      //       component: () => import('@/views/product/list/Index.vue'),
+      //       meta: {
+      //         title: '产品列表',
+      //       },
+      //     },
+      //     {
+      //       path: 'category',  //加/跳转时会从站点的根目录开始寻找，而不加/则会从当前路径开始寻找
+      //       name: 'category',
+      //       component: () => import('@/views/product/category/Index.vue'),
+      //       meta: {
+      //         title: '产品分类',
+      //       },
+      //     },
+      //     {
+      //       path: 'add-product',  //商品添加-编辑-查看
+      //       name: 'ProductPage',
+      //       component: () => import('@/views/product/list/AddProduct.vue'),
+      //       meta: {  //配置高亮标识
+      //         activeMenu: '/product/list',
+      //         title: '商品信息编辑'
+      //       }
+      //     },
+      //   ]
+      // },
+      // {
+      //   path: '/order',
+      //   name: 'order',
+      //   component: () => import('@/views/order/Index.vue'),
+      //   redirect: '/order/collect',
+      //   meta: {
+      //     title: '订单管理',
+      //   },
+      //   children: [
+      //     {
+      //       path: 'collect',
+      //       name: 'collect',
+      //       component: () => import('@/views/order/collect/Index.vue'),
+      //       meta: {
+      //         title: '订单汇总',
+      //       },
+      //     },
+      //     {
+      //       path: 'contract',
+      //       name: 'contract',
+      //       component: () => import('@/views/order/contract/Index.vue'),
+      //       meta: {
+      //         title: '订单审核',
+      //       },
+      //     },
+      //     {
+      //       path: 'order-list',
+      //       name: 'orderList',
+      //       component: () => import('@/views/order/list/Index.vue'),
+      //       meta: {
+      //         title: '订单列表',
+      //       },
+      //     },
+      //   ]
+      // },
+      // {
+      //   path: '/advert',
+      //   name: 'advert',
+      //   component: () => import('@/views/advert/Index.vue'),
+      //   // redirect: '/advert/list',
+      //   meta: {
+      //     title: '广告管理',
+      //   },
+      //   children: [
+      //     {
+      //       path: 'advert-list',
+      //       name: 'advertList',
+      //       component: () => import('@/views/advert/list/Index.vue'),
+      //       meta: {
+      //         title: '广告列表',
+      //       },
+      //     },
+      //   ]
+      // },
+      // { 
+      //   path:'/system',
+      //   name:'system',
+      //   component: () => import('@/views/SystemMange/Index.vue'),
+      //   redirect:'/system/role',
+      //   meta:{
+      //     title:"系统管理",
+      //   },
+      //   children:[
+      //     {
+      //       path:'role',
+      //       name:'role',
+      //       component: () => import('@/views/SystemMange/role/Index.vue'),
+      //       meta:{
+      //         title:"角色管理",
+      //       },
+      //     },
+      //     {
+      //       path:'department',
+      //       name:'department',
+      //       component: () => import('@/views/SystemMange/department/Index.vue'),
+      //       meta:{
+      //         title:"部门管理",
+      //       },
+      //     }
+      //   ]
+    
+      // },
+    // ]
+  // },
   {
     path: '/login',
     name: 'Login',
--- src/store/modules/menu.js
+++ src/store/modules/menu.js
@@ -1,7 +1,10 @@
 /* 动态的路由导航 */
 //导入请求的接口方法
 import {permission} from '@/api/index'
-
+//导入前端定义好的完整的路由信息
+import { menu } from '@/router/menu'
+//导入路由 和基本的路由信息
+import router,{baseRoutes} from '@/router/index'
 export default {
     namespaced: true,
     state: {
@@ -24,6 +27,7 @@ export default {
           // console.log('rootState.login.userinfo.token',rootState.login.userinfo.token);
           let res = await permission({token:rootState.login.userinfo.token})
           console.log('后端返回的导航菜单内容:----',res.data.data);
+          console.log('前端定义的导航菜单内容-----',menu);
         }
     }
   };
